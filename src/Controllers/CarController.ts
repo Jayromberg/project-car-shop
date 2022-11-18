81e4fcb5fd6fdee1ff7d3b1229ea90fc71d9603a
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-18 11:55:17 -0300

    feat: Ajustes no controller e services

---
+++ src/Controllers/CarController.ts
@@ -0,0 +1,36 @@
+import { NextFunction, Request, Response } from 'express';
+import { ICar } from '../Interfaces/ICar';
+import CarInstance from '../Instances/CarInstance';
+
+class CarController {
+  private req: Request;
+  private res: Response;
+  private next: NextFunction;
+
+  constructor(req: Request, res: Response, next: NextFunction) {
+    this.req = req;
+    this.res = res;
+    this.next = next;
+  }
+
+  public async create() {
+    const car: ICar = {
+      model: this.req.body.model,
+      year: this.req.body.year,
+      color: this.req.body.color,
+      status: this.req.body.status || undefined,
+      buyValue: this.req.body.buyValue,
+      doorsQty: this.req.body.doorsQty,
+      seatsQty: this.req.body.seatsQty, 
+    };
+
+    try {
+      const newCar = await CarInstance.main().register(car);
+      return this.res.status(201).json(newCar);
+    } catch (error) {
+      this.next(error);
+    }
+  }
+}
+
+export default CarController;
