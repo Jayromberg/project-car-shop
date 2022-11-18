27ea9a1a32469714540d91cf95393f5d1214f8f4
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-18 14:49:25 -0300

    feat: Middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './Routes';
+import ErrorHandler from './Middlewares/ErrorHandler';
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(ErrorHandler.handle);
 
 export default app;
