eddfbc4a3fadc5d05f17afb47d4edeca6f441e87
Jayromberg Lima Santos <jayro@pop-os.localdomain>
2022-11-17 22:47:24 -0300

    feat: Implememtação da model e sua abstração

---
+++ src/Models/AbstractODM.ts
@@ -0,0 +1,26 @@
+import {
+  // isValidObjectId,
+  Model,
+  models,
+  Schema,
+  // UpdateQuery,
+  model,
+} from 'mongoose';
+  
+abstract class AbstractODM<T> {
+  protected model: Model<T>;
+  protected schema: Schema;
+  protected modelName: string;
+  
+  constructor(schema: Schema, modelName: string) {
+    this.schema = schema;
+    this.modelName = modelName;
+    this.model = models[this.modelName] || model(this.modelName, this.schema);
+  }
+  
+  public async create(obj: T): Promise<T> {
+    return this.model.create({ ...obj });
+  }
+}
+  
+export default AbstractODM;
